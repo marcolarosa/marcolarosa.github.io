94599c307a1d931b6357b4ebe0e78bfb561df507
Marco La Rosa <m@lr.id.au>
2023-11-16 15:06:12 +1100

    wire up slideshow component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-    <h1>Index page</h1>
+    <div>
+        <SlideShowComponent id="deck1" style="height: 500px" class="m-4">
+            <section>slide 1</section>
+            <section>slide 2</section>
+        </SlideShowComponent>
+
+        <SlideShowComponent id="deck2" style="height: 500px" class="m-4">
+            <section>deck 1</section>
+            <section>deck 2</section>
+        </SlideShowComponent>
+    </div>
 </template>
+
+<script setup>
+import SlideShowComponent from "../src/components/slideshow.vue";
+</script>
