f434a3d17643733cae741b78024cd5a6e7872951
Marco La Rosa <m@lr.id.au>
2023-11-29 16:18:32 +1100

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    future: {},
+    mode: "jit",
+    content: ["./public/index.html", "./src/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+};
