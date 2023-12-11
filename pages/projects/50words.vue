d04eaf629ccfae2e914c424a44197f11efb35108
Marco La Rosa <m@lr.id.au>
2023-12-12 10:59:38 +1100

    write 50words content

--- pages/projects/50words.vue
+++ pages/projects/50words.vue
@@ -1,1 +1,81 @@
-<template></template>
+<template>
+    <stack>
+        <card>
+            <template #text>
+                <div class="text-center text-2xl">
+                    <linker to="https://50words.online">50words</linker>
+                </div>
+            </template>
+        </card>
+
+        <card>
+            <template #text>
+                This project from the Research Unit for Indigenous Language aims to provide fifty
+                words in Indigenous languages of Australia. The map is intended to be a useful
+                resource for schools and educational organisations to learn 50 words in their local
+                languages, and for the general public to discover the diversity of languages around
+                Australia.
+            </template>
+        </card>
+
+        <card>
+            <template #text>
+                <div class="flex flex-col space-y-2">
+                    <p>I designed and built the 50words application.</p>
+                    <ul class="list-disc">
+                        <li>
+                            The application is a VueJS SPA with a Python data processing backend.
+                        </li>
+                        <li>
+                            Project managers collect recordings of native speakers speaking the
+                            words and upload the WAV files to the server along with an Excel
+                            spreadsheet containing the metadata.
+                        </li>
+
+                        <li>
+                            They then run a python script which creates an mp3 file for each wav
+                            file and a set of JSON index files.
+                        </li>
+                        <li>
+                            When the application loads, it loads the main index file which then has
+                            links to the sub index files. In this way, the application itself can
+                            run as an SPA without an API server. It also means the application
+                            scales with the data rather than slowing down as number of languages
+                            increases.
+                        </li>
+                        <li><linker to="https://github.com/coedl/50words.online"></linker></li>
+                    </ul>
+                </div>
+            </template>
+        </card>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <card>
+                <template #image>
+                    <img src="/50words/50words1.png" class="object-contain" />
+                </template>
+            </card>
+            <card>
+                <template #image>
+                    <img src="/50words/50words2.png" class="object-contain" />
+                </template>
+            </card>
+            <card>
+                <template #image>
+                    <img src="/50words/50words3.png" class="object-contain" />
+                </template>
+            </card>
+            <card>
+                <template #image>
+                    <img src="/50words/50words4.png" class="object-contain" />
+                </template>
+            </card>
+        </div>
+    </stack>
+</template>
+
+<script setup>
+import linker from "~/src/components/linker.vue";
+import stack from "~/src/components/stack.vue";
+import card from "~/src/components/Card.vue";
+</script>
--- pages/projects/modpdsc.vue
+++ pages/projects/modpdsc.vue
@@ -1,1 +1,7 @@
 <template></template>
+
+<script setup>
+import linker from "~/src/components/linker.vue";
+import stack from "~/src/components/stack.vue";
+import card from "~/src/components/Card.vue";
+</script>
