0593d77767fa5d8523777630342d23d0db0ff449
Marco La Rosa <m@lr.id.au>
2023-12-12 08:13:11 +1100

    add icon to show external link

--- src/components/linker.vue
+++ src/components/linker.vue
@@ -1,7 +1,10 @@
 <template>
-    <NuxtLink :to="props.to" target="_blank"
-        ><slot v-if="slots._"></slot><span v-else>{{ props.to }}</span></NuxtLink
-    >
+    <NuxtLink :to="props.to" target="_blank" class="text-blue-700">
+        <slot v-if="slots._"> {{ slots._ }}</slot>
+        <span v-else>
+            {{ props.to }}&nbsp;<i class="fa-solid fa-arrow-up-right-from-square"></i>
+        </span>
+    </NuxtLink>
 </template>
 
 <script setup>
